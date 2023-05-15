3077cadff39118d7e13e1a95f0a7b77090d73e36
Cawe Alves <cawe.wewe04@gmail.com>
2023-05-15 17:50:43 -0300

    feat: food model

---
+++ poemon/src/main/java/com/cawe/poemon/model/Food.java
@@ -0,0 +1,10 @@
+package com.cawe.poemon.model;
+
+import java.text.DecimalFormat;
+
+public class Food {
+    private String name;
+    private String type;
+    private String color;
+    private DecimalFormat giveXP;
+}
