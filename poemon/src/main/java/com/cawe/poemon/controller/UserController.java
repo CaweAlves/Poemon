c54bd86674c28cdd313ef7393d38c7cefb6c505d
Cawe541 <cawe.wewe04@gmail.com>
2023-05-17 00:01:03 -0300

    fix: method name

--- poemon/src/main/java/com/cawe/poemon/controller/UserController.java
+++ poemon/src/main/java/com/cawe/poemon/controller/UserController.java
@@ -3,8 +3,6 @@ package com.cawe.poemon.controller;
 import com.cawe.poemon.model.User;
 import com.cawe.poemon.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +14,7 @@ public class UserController {
 
     @RequestMapping("/login")
     public ResponseEntity<User> login(@PathVariable String email, @PathVariable String password) {
-        return new ResponseEntity<User>(service.VerifyUserCredencials(email, password));
+        return new ResponseEntity<User>(service.VerifyUserCredentials(email, password));
     }
 
 }
--- poemon/src/main/java/com/cawe/poemon/service/UserService.java
+++ poemon/src/main/java/com/cawe/poemon/service/UserService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    public HttpStatus VerifyUserCredencials(String email, String password) {
+    public HttpStatus VerifyUserCredentials(String email, String password) {
         return new UserDAO().isUserValid(email, password) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
     }
 }
