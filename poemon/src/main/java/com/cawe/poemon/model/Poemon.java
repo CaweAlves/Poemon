378eabab13090ecf148ef5df37f2d020d96721fd
Cawe Alves <cawe.wewe04@gmail.com>
2023-06-25 14:52:21 -0300

    update(poemon): add serialize and column annotation

--- poemon/src/main/java/com/cawe/poemon/model/Poemon.java
+++ poemon/src/main/java/com/cawe/poemon/model/Poemon.java
@@ -4,23 +4,31 @@ import com.cawe.poemon.enums.TypeEnum;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Getter
 @Entity
 @Table(name = "poemon")
-public class Poemon implements ModelInterface{
+public class Poemon implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private UUID id;
+    @Column(nullable = false, length = 40)
     private String name;
+    @Column(nullable = false)
     private TypeEnum type;
+    @Column(nullable = false)
     private String color;
+    @Column(nullable = false)
     private int level;
+    @Column(nullable = false)
     private BigDecimal xp;
+    @Column(nullable = false)
     private BigDecimal nextLevel;
     
     public void setName(String name) {this.name = name;}
-    public void setcolor(String color) {this.color = color;}
+    public void setColor(String color) {this.color = color;}
     public void setType(TypeEnum type) {this.type = type;}
 }
