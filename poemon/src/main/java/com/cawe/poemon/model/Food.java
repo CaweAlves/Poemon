87c4ff58e881d2251b018aa5f507df591520d323
Cawe Alves <cawe.wewe04@gmail.com>
2023-06-25 14:52:02 -0300

    update(food): add serialize and column annotation

--- poemon/src/main/java/com/cawe/poemon/model/Food.java
+++ poemon/src/main/java/com/cawe/poemon/model/Food.java
@@ -4,17 +4,23 @@ import com.cawe.poemon.enums.TypeEnum;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Getter
 @Entity
 @Table(name = "food")
-public class Food implements ModelInterface {
+public class Food implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private UUID id;
+    @Column(nullable = false, length = 40)
     private String name;
+    @Column(nullable = false)
     private TypeEnum type;
+    @Column(nullable = false)
     private String color;
+    @Column(nullable = false)
     private BigDecimal giveXP;
 }
