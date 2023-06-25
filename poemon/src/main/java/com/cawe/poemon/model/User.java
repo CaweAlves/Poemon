39a2312adb0146ebbab1e9b92cb76092b7b5b0b6
Cawe Alves <cawe.wewe04@gmail.com>
2023-06-25 14:52:28 -0300

    update(user): add serialize and column annotation

--- poemon/src/main/java/com/cawe/poemon/model/User.java
+++ poemon/src/main/java/com/cawe/poemon/model/User.java
@@ -4,18 +4,24 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "user")
-public class User implements ModelInterface {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private UUID id;
+    @Column(nullable = false, length = 250)
     private String name;
+    @Column(nullable = false, unique = true, length = 150)
     private String email;
+    @Column(nullable = false, length = 100)
     private String password;
+    @Column(nullable = false)
     private Date birth;
 }
